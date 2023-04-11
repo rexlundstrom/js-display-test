14e0f9d59c294e7164589b7e4afaa6b831ea5de8
Rex Lundstrom <rexlundstrom@gmail.com>
2023-04-11 17:58:30 -0400

    app functionality added

--- js/site.js
+++ js/site.js
@@ -1,13 +1,45 @@
-function displayMessage() {
-    let msg = document.getElementById('message').value;
-    // alert(msg);
+// get start/end numbers from inputs
+// Entry point AKA Controller function
+function getValues() {
+  let startValue = document.getElementById("startValue").value;
+  let endValue = document.getElementById("endValue").value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'APP NAME',
-            text: msg
-        }
-    );
-}
+  let startNumber = parseInt(startValue);
+  let endNumber = parseInt(endValue);
 
+  let numbersArray = generateNumbers(startNumber, endNumber);
+  displayNumbers(numbersArray)
+};
+
+
+
+// generate numbers to display
+// business/logic function
+function generateNumbers(start, end) {
+
+  let numbersArray = [];
+
+  for (let i = start; i <= end; i++) {
+    numbersArray.push(i);
+  }
+
+  return numbersArray;
+};
+
+// places generated numbers on a page AND bolding even numbers
+// View function
+function displayNumbers (numbers) {
+
+  let results = '';
+
+  for (let i = 0; i < numbers.length; i++) {
+    let currNumber = numbers[i];
+
+    results = results + '<tr><td>' + currNumber + "</td></tr>";
+
+  }
+
+
+  let tableBody = document.getElementById('results');
+  tableBody.innerHTML = results;
+};
